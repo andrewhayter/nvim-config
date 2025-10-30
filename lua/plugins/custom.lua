c15354dff6bc9344fb1fff09fa7664959918769b
Andrew Hayter <andrewhayter@gmail.com>
2025-10-30 10:57:57 -0400

    Initial commit: Mostly standard LazyVim config with TypeScript, React, Tailwind, Rust, Solana
    - Nightfox colorscheme
    - Full web stack: TypeScript, React, Next.js, Tailwind
    - Backend: Python, Go, Rust
    - Auto-formatting and linting
    - Custom keybindings for buffer navigation

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,297 @@
+return {
+  -- ============================================================================
+  -- THEME CONFIGURATION
+  -- ============================================================================
+  -- Nightfox colorscheme
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      options = {
+        transparent = false,
+        terminal_colors = true,
+        dim_inactive = false,
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        },
+      },
+    },
+  },
+
+  -- Configure LazyVim to use nightfox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfox",
+    },
+  },
+
+  -- ============================================================================
+  -- LAZYVIM EXTRAS - MUST COME FIRST
+  -- ============================================================================
+  -- Language Support Extras
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+
+  -- ============================================================================
+  -- CUSTOM PLUGIN CONFIGURATIONS
+  -- ============================================================================
+
+  -- Trouble: Better diagnostics display
+  {
+    "folke/trouble.nvim",
+    opts = { use_diagnostic_signs = true },
+  },
+
+  -- which-key: Keybinding helper
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+
+  -- Noice: Better UI for messages, cmdline and popupmenu
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      notify = {
+        enabled = true,
+      },
+      lsp = {
+        hover = { enabled = true },
+        signature = { enabled = true },
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+
+  -- Telescope: Fuzzy finder with better defaults
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
+
+  -- LSP: Language server configuration
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        pyright = {},
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                buildScripts = {
+                  enable = true,
+                },
+              },
+              checkOnSave = {
+                command = "clippy",
+                extraArgs = { "--all-targets" },
+              },
+              procMacro = {
+                enable = true,
+              },
+            },
+          },
+        },
+        emmet_ls = {
+          filetypes = {
+            "html",
+            "css",
+            "scss",
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+          },
+        },
+      },
+    },
+  },
+
+  -- Treesitter: Better syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "c",
+        "css",
+        "go",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "scss",
+        "sql",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "toml",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+        },
+      },
+    },
+  },
+
+  -- Rainbow delimiters: Colorful bracket matching
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("rainbow-delimiters.setup").setup({})
+    end,
+  },
+
+  -- Tailwind CSS colorizer
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = {
+      filetypes = {
+        "typescript",
+        "typescriptreact",
+        "javascript",
+        "javascriptreact",
+        "css",
+        "scss",
+        "html",
+      },
+      user_default_options = {
+        tailwind = true,
+        css = true,
+        css_fn = true,
+      },
+    },
+  },
+
+  -- Lualine: Statusline with nightfox theme
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "nightfox",
+      },
+    },
+  },
+
+  -- Mason: Tool installer
+  {
+    "mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "prettier",
+        "rustywind",
+        "shfmt",
+        "eslint_d",
+        "shellcheck",
+        "ruff",
+        "css-lsp",
+        "html-lsp",
+        "emmet-ls",
+      },
+    },
+  },
+
+  -- Auto-close HTML/JSX tags
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    opts = {},
+  },
+
+  -- Bufferline: Better buffer/tab bar
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+
+  -- Package.json helper
+  {
+    "vuki656/package-info.nvim",
+    event = "BufRead package.json",
+    config = function()
+      require("package-info").setup()
+    end,
+  },
+}
